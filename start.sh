717d0be5bc01d9c989c70d1af7b6169651c8b6e1
resavel <80557733+resavel@users.noreply.github.com>
2021-03-13 05:49:24 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgnwlcffsynf0gr7cewsnzv7rvm4u27q0patvy9.test1
 
 
 done
